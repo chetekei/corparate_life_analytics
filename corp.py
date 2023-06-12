b38a546e8a86c9387435b7cc4df5357c14f1a444
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-06-12 15:21:55 +0300

    Update corp.py

--- corp.py
+++ corp.py
@@ -24,8 +24,8 @@ uploaded_file = st.sidebar.file_uploader(
 if uploaded_file is not None:
     try:
         if uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-            data_types = {'Policy No': str}
-            df = pd.read_excel("data.xlsx", dtype = data_types , header = 3)
+            
+            df = pd.read_excel("data.xlsx", header = 3)
            
         elif uploaded_file.type == "text/csv":
             df = pd.read_csv(uploaded_file, header=3)
