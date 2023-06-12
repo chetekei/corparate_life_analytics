e7aadfd1dbfe7bdff79e261ef0cb038936d66e92
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-06-12 15:05:47 +0300

    Update corp.py

--- corp.py
+++ corp.py
@@ -122,11 +122,9 @@ if uploaded_file is not None:
         # Display the DataFrame
         st.subheader("Maturity in next 90 days")
         
-        st.markdown(Matured_policies_90,unsafe_allow_html=True )
-        
+        st.markdown(Matured_policies_90, unsafe_allow_html=True )        
    
-    else:
-        st.write("Invalid chart selection.")
+
 elif uploaded_file is None and chart_select == "Maturity in next 30 days":
     st.write("Please upload a file.")
              
