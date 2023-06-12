babc0afca551e2d945054c9a5b2085db5c6bdd20
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-06-12 15:34:17 +0300

    Update corp.py

--- corp.py
+++ corp.py
@@ -25,10 +25,10 @@ if uploaded_file is not None:
     try:
         if uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
             data_types = {'Policy No': str}
-            df = pd.read_excel(uploaded_file, dtype = data_types , header = 3)
+            df = pd.read_excel(uploaded_file, dtype = data_types , header = 4)
                       
         elif uploaded_file.type == "text/csv":
-            df = pd.read_csv(uploaded_file, header=3)
+            df = pd.read_csv(uploaded_file, header=4)
             
         
         df.dropna(subset=['Policy No'], inplace = True)
